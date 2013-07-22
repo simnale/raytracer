a3882874d5a178c8930b33c12f3c1f0e4c27aec2
Alessandro Simon <simnale@gmail.com>
2013-07-22 18:21:35 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <FreeImage.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Usage: raycaster test" << endl;
+    return 0;
+}
